51bd5a954b14ced52aec42e99e3dd3e1dcbe9856
Shane Thurston <36428573+shane-thurston@users.noreply.github.com>
2020-03-23 09:28:29 +1100

    Create small_turn.py

---
+++ small_turn.py
@@ -0,0 +1,16 @@
+from pybricks import ev3brick as brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import (Port, Stop, Direction, Button, Color,
+                                 SoundFile, ImageFile, Align)
+from pybricks.tools import print, wait, StopWatch
+from pybricks.robotics import DriveBase
+
+# Write your program here
+left = Motor(Port.B)
+right = Motor(Port.C)
+robot = DriveBase(left,right,56,114)
+
+#(90 degrees)
+robot.drive_time(0, 90, 1000)
+robot.stop()
