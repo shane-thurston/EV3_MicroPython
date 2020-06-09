85aeac131de8f8fd5c2b07e197479aee6ec9c4c2
Shane Thurston <36428573+shane-thurston@users.noreply.github.com>
2020-06-09 12:47:27 +1000

    Create M1_communications.py

---
+++ M1_communications.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env pybricks-micropython
+
+from pybricks import ev3brick as brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import (Port, Stop, Direction, Button, Color,
+                                 SoundFile, ImageFile, Align)
+from pybricks.tools import print, wait, StopWatch
+from pybricks.robotics import DriveBase
+
+# Write your program here
+left = Motor(Port.B)
+right = Motor(Port.C)
+robot = DriveBase(left,right,56,114)
+
+#drive_time(speed:mm/s, steering:deg/s, time:ms)
+#Drive 25cm - 2.5 seconds at 100 speed
+robot.drive_time(100, 0, 3600)
+robot.drive_time(0, -55, 1000)
+robot.drive_time(100, 0, 2500)
+robot.stop()
+
+#You need to perform the same actions in reverse to complete challenge
