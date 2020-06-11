6045894c0eb9d9351f086d077097ff202e94442e
Shane Thurston <36428573+shane-thurston@users.noreply.github.com>
2020-06-11 13:32:48 +1000

    Create arm_motor.py

---
+++ arm_motor.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env pybricks-micropython
+
+from pybricks import ev3brick as brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import (Port, Stop, Direction, Button, Color,
+                                 SoundFile, ImageFile, Align)
+from pybricks.tools import print, wait, StopWatch
+from pybricks.robotics import DriveBase
+
+# Setup drive base
+left = Motor(Port.A)
+right = Motor(Port.B)
+robot = DriveBase(left,right,56,114)
+
+# Setup arm motor
+arm_motor = Motor(Port.C)
+
+#drive_time(speed:mm/s, steering:deg/s, time:ms)
+robot.drive_time(100, 0, 1000) #Drive forward 10cm
+
+robot.stop()
+
+#run arm motor
+arm_motor.run_angle(100,60)
