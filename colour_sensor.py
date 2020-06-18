49300b3d0b835545d9a364d4ccc1ecd98b3ed6e8
Shane Thurston <36428573+shane-thurston@users.noreply.github.com>
2020-06-18 11:20:50 +1000

    Create colour_sensor.py

---
+++ colour_sensor.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env pybricks-micropython
+
+from pybricks import ev3brick as brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import (Port, Stop, Direction, Button, Color,
+                                 SoundFile, ImageFile, Align)
+from pybricks.tools import print, wait, StopWatch
+from pybricks.robotics import DriveBase
+
+# Write your program here
+left = Motor(Port.B)
+right = Motor(Port.C)
+robot = DriveBase(left,right,56,114)
+
+colour_sensor = ColorSensor(Port.S1)
+while obstacle_sensor.reflection() < 50:
+  robot.drive(200,0)
+robot.stop()
